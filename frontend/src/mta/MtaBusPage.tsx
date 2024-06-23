e6ae26fdc00e64f6615d7c38e4067c8daf25cf57
Steven Tung <steven.tung.1@stonybrook.edu>
2024-06-23 16:30:38 -0400

    Added Information Pulling by StopID

--- frontend/src/mta/MtaBusPage.tsx
+++ frontend/src/mta/MtaBusPage.tsx
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const MtaBusPage = () => {
@@ -9,7 +9,7 @@ const MtaBusPage = () => {
     try {
       const response = await axios.get(`http://localhost:9999/bus`);
       setBackendData(response.data);
-      console.log(backendData)
+      
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -17,25 +17,43 @@ const MtaBusPage = () => {
 
   useEffect(() => {
     fetchData();
-    const interval = setInterval(fetchData, 60000); // Fetch data every 30 seconds
+    const interval = setInterval(fetchData, 30000); // Fetch data every 30 seconds
 
     return () => clearInterval(interval); // Cleanup interval on component unmount
   }, []);
 
-  return <div>
-  {backendData ? (
-      <div>
-        {backendData.Siri.ServiceDelivery.StopMonitoringDelivery.map((stop) => (
-          <div key={stop.MonitoredStopVisit[0].MonitoredVehicleJourney.VehicleRef}>
-            <h2>{stop.MonitoredStopVisit[0].MonitoredVehicleJourney.LineRef}</h2>
-            <p>Next bus: {stop.MonitoredStopVisit[0].MonitoredVehicleJourney.MonitoredCall.ExpectedArrivalTime}</p>
-          </div>
-        ))}
-      </div>
-    ) : (
-      <p>Loading...</p>
-    )}
-  </div> 
-  };
+  useEffect(() => {
+    console.log("HERE", backendData);
+  }, [backendData]);
+  
+  return(
+  <div>
+      {backendData ? (
+        <div>
+          <p>Data Received Time: {backendData.DataReceivedTime}</p>
+          {backendData.MonitoredStopVisit.map((bus, index) => (
+            <div key={index}>
+              --------------------------------------------------------------------------------------------------
+              <h2>{bus.PublishedLineName}, Vehicle {bus.VehicleNumber}</h2>
+              <p>Destination: {bus.DestinationName}, Direction: {bus.DirectionRef}</p>
+              <p>Expected Arrival Time: {bus.ExpectedArrivalTime}</p>
+              <p>Aimed Arrival Time: {bus.AimedArrivalTime}</p>
+              <p>Expected Departure Time: {bus.ExpectedDepartureTime}</p>
+              <p>Aimed Departure Time: {bus.AimedDepartureTime}</p>
+              <p>Capacity: {bus.EstimatedPassengerCount}/{bus.EstimatedPassengerCapacity}</p>
+              <p>Current Position: ({bus.Longitude}, {bus.Latitude}), Bearing: {bus.Bearing}</p>
+              <p>Monitored?: {bus.isMonitored === true? "No" : "Yes"}, Progress Rate: {bus.ProgressRate}</p>
+              <p>{bus.PresentableDistance}, Stops Away: {bus.StopsFromCall}, CallDistanceAlongRoute: {bus.CallDistanceAlongRoute}</p>
+              <p>Stroller Allowed: {bus.StrollerBehicle === true ? "No" : "Yes"}</p>
+            
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  )
+};
   
   export default MtaBusPage;
