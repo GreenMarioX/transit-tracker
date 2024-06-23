e263d9040ce6062c790616cc0d9d5e40d8206222
Steven Tung <steven.tung.1@stonybrook.edu>
2024-06-23 16:40:43 -0400

    Pushed Stop Info Backend

--- backend/controllers/mta/bus.js
+++ backend/controllers/mta/bus.js
@@ -1,28 +1,28 @@
-
+const cleanMTABusData = require('./helpers/bus-data-cleaner');
 
 let busData = null;
 
-const fetchBusData = async () => {
+const fetchBusData = async (stopID) => {
   try {
-    const response = await fetch('https://bustime.mta.info/api/siri/stop-monitoring.json?key=b1af2818-ea0d-4b2f-b632-5119632b6ae3&OperatorRef=MTA&MonitoringRef=503551');
+    const response = await fetch('https://bustime.mta.info/api/siri/stop-monitoring.json?key=b1af2818-ea0d-4b2f-b632-5119632b6ae3&OperatorRef=MTA&MonitoringRef=' + stopID);
     if (!response.ok) {
       throw new Error('Network response was not ok');
-    }
+    }   
     busData = await response.json();
-    console.log(busData)
+    // console.log(cleanMTABusData(busData))
   } catch (error) {
     console.error('Fetching data failed', error);
   }
 };
 
 // Fetch data initially and then every 30 seconds
-fetchBusData();
-setInterval(fetchBusData, 30000);
+fetchBusData(502184);
+setInterval(() => fetchBusData(502184), 30000);
 
 exports.getBusData = (req, res) => {
   if (busData) {
-    console.log(busData.Siri.ServiceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit[0])
-    res.json(busData);
+    const cleanedData = cleanMTABusData(busData);
+    res.json(cleanedData);
   } else {
     res.status(503).send('Data not available');
   }
